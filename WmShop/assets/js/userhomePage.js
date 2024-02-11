44621b4b351b0562bc210bbad9167b390b0a314a
FirstName Falcatan <kmfalcatan2@gmail.com>
2024-02-12 01:20:13 +0800

    akdjbaksd

---
+++ WmShop/assets/js/userhomePage.js
@@ -0,0 +1,16 @@
+function filterItems() {
+    var input, filter, container, items, itemName, i;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    container = document.getElementById('itemContainer');
+    items = container.getElementsByClassName('itemContainer');
+
+    for (i = 0; i < items.length; i++) {
+        itemName = items[i].getElementsByClassName('itemName')[0];
+        if (itemName.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            items[i].style.display = '';
+        } else {
+            items[i].style.display = 'none';
+        }
+    }
+}
